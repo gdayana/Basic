cc87a77fb3fcd313e5357547b8597eb612ab09c0
Andreea Gudac <andreea.gudac@student.upt.ro>
2018-04-12 13:01:22 +0300

    Added Entity Framework (SQL Server+Tools), dependency Injection, and Microsoft.DotNet.Watcher.Tools (for making any changes online during application run)

--- Basic/ClientApp/app/components/app/app.component.ts
+++ Basic/ClientApp/app/components/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MdDialog, MdDialogRef, MdSnackBar } from '@angular/material';
+import { Http } from '@angular/http'
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private _httpService: Http) { }
+    apiValues: string[] = [];
+    ngOnInit() {
+        this._httpService.get('/api/values').subscribe(values => {
+            this.apiValues = values.json() as string[];
+        });
+    }
 }
